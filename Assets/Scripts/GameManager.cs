6c85e7d1fb68081799f060593e457634c0a38cb1
Lucas Foxworthy <40660037+EverWand@users.noreply.github.com>
2024-06-25 03:55:55 -0700

    GameManager Tracks AIControllers in List

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,6 +8,10 @@ public class GameManager : MonoBehaviour
     public GameObject playerControllerPrefab;
     public GameObject playerPrefab;
     public Transform playertransform;
+    private GameObject playerCharacter;
+
+    //AIControllers
+    private List<AIController> AIControllerList;
 
     public void Awake()
     {
@@ -17,20 +20,53 @@ public class GameManager : MonoBehaviour
             instance = this;
             DontDestroyOnLoad(gameObject);
         }
-        else { 
+        else
+        {
             Destroy(gameObject);
         }
     }
     public void Start()
     {
-        SpawnPlayer();
+        SpawnPlayer(); //Spawn the Player into the Scene
+
+        try //catch any errors when setting default target to all AI's
+        {
+            SetDefaultAITarget(playerCharacter); //set default AI Target to all AIs
+        }
+        catch { };
     }
-    public void SpawnPlayer() {
+    //instantiates the Player in the transform of the playerspawner
+    private void SpawnPlayer()
+    {
         //Player set-up References
         GameObject Controller = Instantiate(playerControllerPrefab, Vector3.zero, Quaternion.identity) as GameObject; //Make a Player controller into the scene
-        GameObject playerCharacter = Instantiate(playerPrefab, playertransform) as GameObject; //Make a Player Pawn into the scene
+        playerCharacter = Instantiate(playerPrefab, playertransform) as GameObject; //Make a Player Pawn into the scene
         playerCharacter.AddComponent<NoiseMaker>();
 
         Controller.GetComponent<Controller>().pawn = playerCharacter.GetComponent<Pawn>(); // Attatch the spawned player pawn to the spawned controller
+    }
+
+    //Set the DeafaultAITargets to a specific target
+    private void SetDefaultAITarget(GameObject target)
+    {
+        AIControllerList = UpdateAIControllerList(); //Get List of AIControllers
+        
+        //For every AI Controller Found...
+        foreach (AIController AI in AIControllerList)
+        {
+            //and if that AIController doesn't have a target Set...
+            if (AI.target == null)
+            {
+                //Set it to the specific target
+                AI.target = target;
+            }
+        }
+    }
+
+    //Updates the List of AIControllers
+    public List<AIController> UpdateAIControllerList() 
+    {
+        AIController[] foundControllers = FindObjectsByType<AIController>(FindObjectsSortMode.None); //find all AIController instances and save them into an array
+        return new List<AIController>(foundControllers);    //Return the Controller array as a list 
     }
 }
